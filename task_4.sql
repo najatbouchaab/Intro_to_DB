297d5b4e2303b7c8b8f185617dad80d48d04c81f
najatbouchaab <bouchaabnajat33@gmail.com>
2025-10-16 01:52:50 +0200

    Add SQL script to insert single row into customers table

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,7 @@
 -- Display the full description of the table 'books'
 SHOW FULL COLUMNS FROM books;
+-- Print full description of the table 'books' without using DESCRIBE, EXPLAIN, or ANALYZE
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'books'
+  AND TABLE_SCHEMA = DATABASE();
