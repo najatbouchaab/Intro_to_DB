42d0792bd4a9a3aa32848dfe33844d863a423a34
najatbouchaab <bouchaabnajat33@gmail.com>
2025-10-17 00:42:59 +0200

    week 8

--- task_6.sql
+++ task_6.sql
@@ -1,17 +1,4 @@
--- This script inserts multiple new records into the 'customer' table of the alx_book_store database.
-
-INSERT INTO customer (
-    customer_id,
-    customer_name,
-    email,
-    address
-)
-VALUES
-    -- Record 1: Blessing Malik
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-
-    -- Record 2: Obed Ehoneah
-    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-
-    -- Record 3: Nehemial Kamolu
-    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+INSERT INTO customers (customer_id, customer_name, email, address) VALUES
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
