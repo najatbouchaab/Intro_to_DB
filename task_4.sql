d892c0fedd5c267a0ce3831a508e53eda586b83a
najatbouchaab <bouchaabnajat33@gmail.com>
2025-10-17 01:00:34 +0200

    week 8

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,7 @@
 -- Task 4: Full description of the table 'books'
--- This script displays the full structure of the table 'books'
+-- This script retrieves the full description of the 'books' table
 -- without using DESCRIBE, EXPLAIN, or ANALYZE.
 
-SHOW CREATE TABLE books;
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
