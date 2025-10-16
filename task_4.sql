4703ae695aca21c2a0244a62369695c8c5883701
najatbouchaab <bouchaabnajat33@gmail.com>
2025-10-17 00:59:02 +0200

    week 8

--- task_4.sql
+++ task_4.sql
@@ -1,12 +1,5 @@
--- Show full description of Books table without using DESCRIBE or EXPLAIN
-SELECT 
-    COLUMN_NAME, 
-    COLUMN_TYPE, 
-    IS_NULLABLE, 
-    COLUMN_KEY, 
-    COLUMN_DEFAULT, 
-    EXTRA
-FROM 
-    INFORMATION_SCHEMA.COLUMNS
-WHERE 
-    TABLE_NAME = 'Books';
+-- Task 4: Full description of the table 'books'
+-- This script displays the full structure of the table 'books'
+-- without using DESCRIBE, EXPLAIN, or ANALYZE.
+
+SHOW CREATE TABLE books;
