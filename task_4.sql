db951e00683a183b7ae22677eabf7f33f6b2f946
najatbouchaab <bouchaabnajat33@gmail.com>
2025-10-17 00:50:48 +0200

    week 8

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,14 @@
 SHOW CREATE TABLE books;
+-- Show full description of Books table without using DESCRIBE or EXPLAIN
+SELECT 
+    COLUMN_NAME AS 'Field',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books';
