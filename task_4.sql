57a77a31106b39cbb0f0fd3200d38b1d1ffca0bf
najatbouchaab <bouchaabnajat33@gmail.com>
2025-10-17 02:10:03 +0200

    week 8

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,3 @@
-SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT 
+FROM information_schema.COLUMNS 
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
